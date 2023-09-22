52af4f2edd5adf32ebaeb249958e217acf048666
Anton Tyshko <tyshko@neosensory.com>
2023-09-22 12:29:03 -0500

    keep compatibility with Xcode 14

--- Source/Unimplemented.swift
+++ Source/Unimplemented.swift
@@ -1,5 +1,7 @@
 import RxSwift
 
+public typealias Observable = RxSwift.Observable
+
 func unimplementedFunction(file: String = #file, function: String = #function, line: Int = #line) {
     fatalError("Unimplemented function \(function) in \(file):\(line)")
 }
@@ -8,6 +10,6 @@ extension Observable {
     static func unimplemented(file: String = #file, function: String = #function, line: Int = #line)
         -> Observable<Element> {
         unimplementedFunction(file: file, function: function, line: line)
-        return Observable<Element>.empty()
+            return .empty()
     }
 }
